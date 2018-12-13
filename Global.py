8d8eee14729b1f0251d8cfcedaba5a6a041a3b2f
himanshu gurjar <himanshuforforms@gmail.com>
2018-12-13 16:53:10 +0530

    fist commit

---
+++ Global.py
@@ -0,0 +1,152 @@
+# imports
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import mahotas
+import cv2
+import os
+import h5py
+
+# image size
+fixed_size = tuple((500, 500))
+
+#training data path
+train_path = "C:\\Users\\himanshu\\Desktop\\flowers classification\\dataset\\train"
+
+# no. of random forest trees
+num_trees = 100
+
+# bins for histogram
+bins = 0
+
+# train test split size
+test_train = 0.10
+
+# seed for representing same results
+seed = 9 
+
+# first feature descriptor : Hu Moments
+def fd_hu_moments(image):
+	# converting imge to greyscale using cvtColor 
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	# using HuMoments func. to extract Hu moments
+	# argument is a flattened image moments 
+	feature = cv2.HuMoments(cv2.moments(image)).flatten()
+	return feature
+
+# second feature descriptor : Haralick texture
+def fd_haralick(image):
+	# converting to greyscale
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# computing haralick texture feature vector
+	haralick = mahotas.features.haralick(gray).mean(axis = 0)
+	return haralick
+
+# third feature descriptor : Color Histogram
+def fd_histogram(image, mask = None):
+	# converting imge to HSV using cvtColor 
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+	# computing color histogram
+	hist = cv2.calcHist([image], [0, 1, 2], None, [bins, bins, bins], [0, 256, 0, 256, 0, 256])
+	# normalize the histogram
+	hist = cv2.normalize(hist, hist)
+	return hist
+
+
+# getting training labels
+train_labels = os.listdir(train_path)
+
+#sorting training labels
+train_labels.sort()
+print(train_labels)
+
+# emoty list to hold feature vectors and labels
+global_features = []
+labels = []
+
+i, j = 0, 0
+k = 0
+
+# number of images per class
+image_per_class = 80
+
+# loop over training data sub-folders
+for training_name in train_labels:
+    
+    # joiimg the training path athe specific folders
+    dir = os.path.join(train_path, training_name)
+    
+    current_label = training_name
+    
+    k = 1
+    # loop over the images in each sub-folder
+    for x in range(1, image_per_class+1):
+        # getting inage file name
+        file = dir + "/1 (" + str(x) + ").jpg"
+        
+        # read the image and resize it to a fixed-size
+        image = cv2.imread(file)
+        image = cv2.resize(image, (500, 500))
+        
+        #
+        # global feature extraction
+        #
+        
+        fv_hu_moments = fd_hu_moments(image)
+        fv_haralick = fd_haralick(image)
+        #fv_histogram = fd_histogram(image)
+
+        # concatenate global features
+        global_feature = np.hstack([fv_hu_moments, fv_haralick])
+        
+        # updating the list of labels and feature vector
+        labels.append(current_label)
+        global_features.append(global_feature)
+        
+        i += 1
+        k += 1
+    print("[status] processed folder: {}".format(current_label))
+    j  += 1
+    
+print("[status] completed global feature extraction")
+
+# getting the feature vector size
+print("feature vector size {}".format(np.array(global_features).shape))
+
+# training label size
+print("training label {}".format(np.array(labels).shape))
+
+# encoding target labels
+targetNames = np.unique(labels)
+le = LabelEncoder()
+target = le.fit_transform(labels)
+print( "[STATUS] training labels encoded..")
+
+# normalize feature vector
+scaler = MinMaxScaler(feature_range = (0, 1))
+rescaled_features = scaler.fit_transform(global_features)
+print( "[STATUS] feature vector normalized..")
+
+print( "target labels: {}".format(target))
+print("target labels shape: {}".format(target.shape))
+
+
+# save feature vector using HDF5
+h5f_data = h5py.File('C:\\Users\\himanshu\\Desktop\\flowers classification\\output\\data.h5', 'w')
+h5f_data.create_dataset('dataset_1', data = np.array(rescaled_features))
+
+h5f_label = h5py.File('C:\\Users\\himanshu\\Desktop\\flowers classification\\output\\label.h5', 'w')
+h5f_label.create_dataset('dataset_1', data = np.array(target.shape))
+
+h5f_data.close()
+h5f_label.close()
+
+
+print("[STATUS] end of training..")
+
+
+
+
+
+
---
+++ train_test.py
@@ -0,0 +1,121 @@
+# importing necessory packages
+import h5py
+import numpy as np
+import os
+import glob
+import cv2
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.externals import joblib
+
+# creating all the machine learning models
+models = []
+models.append(('LR', LogisticRegression(random_state = 9)))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('RF', RandomForestClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(random_state = 9)))
+
+# variable to hold results and names
+results = []
+names = []
+scoring = "accuracy"
+
+# importing feature vector and trained labels
+h5f_data = h5py.File('C:\\Users\\himanshu\\Desktop\\flowers classification\\output\\data.h5', 'r')
+h5f_label = h5py.File('C:\\Users\\himanshu\\Desktop\\flowers classification\\output\\label.h5', 'r')
+
+global_features_string = h5f_data['dataset_1']
+global_labels_string = h5f_label['dataset_1']
+
+global_features = np.array(global_features_string)
+global_labels = np.array(global_labels_string)
+print(global_labels)
+h5f_data.close()
+h5f_label.close()
+
+# verifying the shape of the features vector and labels
+print("[STATUS] features shape: {}".format(global_features.shape))
+print("[STATUS] labels shape: {}".format(global_labels.shape))
+
+print("[STATUS] training started..")
+
+
+# split the training and testing data
+(trainDataGlobal, testDataGlobal, trainLabelsGlobal, testLabelsGlobal) = train_test_split(np.array(global_features), np.array(global_labels), test_size = 0.10, random_state = 9)
+
+print("[STATUS] splitted the train and test data...")
+print("Train data : {}".format(trainDataGlobal.shape))
+print("Test data : {}".format(testDataGlobal.shape))
+print("Test label : {}".format(testLabelsGlobal.shape))
+print("Train label : {}".format(trainLabelsGlobal.shape))
+
+import warnings
+warnings.formatwarnings('ignore')
+
+# 10-fold cross validation
+for name, model in models:
+    kfold = KFold(n_splits = 10, random_state = 7)
+    cv_results = cross_val_score(model, trainDataGlobal, trainLabelsGlobal, cv = kfold, scoring = scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s : %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+    
+# boxplot algoritham comparison
+fig = pyplot.figure()
+fig.suptitle(" ML algoritham comparison")
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+import matplotlib.pyplot as plt
+from Global.py
+
+# creating random forest model
+clf = RandomForestClassifier(n_estimators=100, random_state = 9)
+
+clf.fit(trainDataGlobal, trainLabelsGlobal)
+
+test_path = "C:\\Users\\himanshu\\Desktop\\flowers classification\\dataset\\test"
+
+for file in glob.glob(test_path + "/*.jpg"):
+    image = cv2.imread(file)
+    
+    image = cv2.resize(image, (500, 500))
+    fv_hu_moments = Global.fd_hu_moments(image)
+    fv_haralick = Global.fd_haralick(image)
+    
+    global_feature = np.hstack([fv_hu_moments, fv_haralick])
+    
+    #predicting label
+    prediction = clf.predict(global_feature.reshape(1, -1))[0]
+    
+    #showing predicted label on image
+    cv2.putText(image, trainLabelsGlobal[prediction], (20, 30), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 255), 3)
+    
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
